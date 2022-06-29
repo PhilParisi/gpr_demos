027f9ae5ac6d403b45c6e14bcc110fb8c432cd3c
Phil Parisi <philsbeginnercode@gmail.com>
2022-06-29 13:26:49 -0400

    update kernel to be ready for setting values to zero beyond the lengthscale (Krasno, 2021)

--- gpr_functions/SqExpKernel.m
+++ gpr_functions/SqExpKernel.m
@@ -11,8 +11,15 @@ if size(x(1,:),2) == 1                              % data contain scalars
 
     % distance between two points
     dist = (x-y);                                   % scalar
-    % kernel value
+    
     k = (hp.sigma^2)*exp( -(dist^2)/(2*hp.L^2) );   % scalar
+
+%     % Dist > Lengthscale, then 0
+%     if dist > hp.L
+%         k = 0;
+%     else % Dist < Lengthscale, kernel value
+%         k = (hp.sigma^2)*exp( -(dist^2)/(2*hp.L^2) );   % scalar
+%     end
 
 end
 
@@ -21,10 +28,19 @@ if size(x(1,:),2) == 2                              % data contain 1x2 vectors
 
     % distance between two points
     dist = sqrt( (x(1)-y(1))^2 + (x(2)-y(2))^2 );   % scalar
-    % kernel value
-    k = (hp.sigma^2)*exp( -(dist^2)/(2*hp.L^2) );   % scalar
+    
+    k = (hp.sigma^2)*exp( -(dist^2)/(2*hp.L^2) );
+
+%     % Dist > Lengthscale, then 0
+%     if dist > hp.L
+%         k = 0;
+%     else % Dist < Lengthscale, kernel value
+%         k = (hp.sigma^2)*exp( -(dist^2)/(2*hp.L^2) );   % scalar
+%     end
     
 end
+
+k = round(k,4);
 
 % Great Resource
 % http://evelinag.com/Ariadne/covarianceFunctions.html
