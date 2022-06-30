0e5bb54447fa05de3ac36b873f8622bdeb8789b5
Phil Parisi <philsbeginnercode@gmail.com>
2022-06-29 21:10:56 -0400

    Updated code with Cholesky Solve, need to figure out sparse kernel

--- gpr_2D/GPR_2D_mainscript.m
+++ gpr_2D/GPR_2D_mainscript.m
@@ -30,12 +30,15 @@ addpath(func_dir);
 % you can do this manually with addpath(.../filepath/gpr_functions) 
     % if below code does not work
 
+%%%%%%%%%%%%%%%%%%
+% IF WE USE SPARSE KERNEL THEN CAN'T DO CHOLESKY SOLVE. ASK KRASNO.
+%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SETUP
 
 % Kernel Hyperparameters [not optimized/trained]
 hp.L = 10;           % lengthscale (high = smoother, low = noisier)
-hp.sigma = 2.5;       % output scale (aka vertical scale or process noise)
+hp.sigma = 2;       % output scale (aka vertical scale or process noise)
 
 % Training Data + Gaussian Noise (aka Raw Data)
 nnum = 100; X_beg = -nnum; X_end = nnum;
@@ -50,30 +53,34 @@ X_Star = [[(-15+X_beg):2:(15+X_end)]'; X];            % vertical array, add trai
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MATRIX CALCS
 
 % Calculate V and Inv(V)                            % depends on training x-points only
-var_sig = 0.8;                                      % Noise (sensor noise upon receiving data)
+var_sig = 1;                                      % Noise (sensor noise upon receiving data)
 W = (var_sig.^2)*eye(nnum);                         % Whitenoise (identity * sigmasquared)
-V = K_Function(X,X,hp) + W;                         % Calculate Covariance Matrix using Kernel
-V_inv = inv(V);
+V = K_Function(X,X,hp,'exact') + W;                % Calculate Covariance Matrix using Kernel
+%V_inv = inv(V);
 
 
 % Generate K Parameters
-K_Star = K_Function(X_Star,X,hp);                      % Calculate K_Star for New Point(s)
-K_StarStar = K_Function(X_Star,X_Star,hp);             % Calculate K_StarStar for New Point(s)
+K_Star = K_Function(X_Star,X,hp,'exact');                      % Calculate K_Star for New Point(s)
+K_StarStar = K_Function(X_Star,X_Star,hp,'exact');             % Calculate K_StarStar for New Point(s)
 
+% Cholesky Decomposition
+L = chol(V,'lower');                                   % Lower triangular cholesky factor
 
-% Calculate Predictions!                            % Finally bring in the training y-points here
-Y_Star_Hat = K_Star * V_inv * Y;                    % Y Predictions (mean values of Gaussians)
-CapSigma_Star = K_StarStar - K_Star * V_inv * K_Star'; % Variance Predictions (gives us mean, var for each pt)
+% Calculate Predictions!                               % Finally bring in the training y-points here
+%Y_Star_Hat = K_Star * V_inv * Y;                       % Y Predictions (mean values of Gaussians)
+Y_Star_Hat = K_Star * CholeskySolve(L,Y);
+%CapSigma_Star = K_StarStar - K_Star * V_inv * K_Star'; % Variance Predictions (gives us mean, var for each pt)
+CapSigma_Star = K_StarStar - K_Star * CholeskySolve(L,K_Star');
 Y_Star_Var = diag(CapSigma_Star);                      % The diagonals store the variances we want!
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LOG MARGINAL LIKELIHOOD
 % How good is our fit?
-
 % note log in compsci means natural log, and matlab's log() is natural log
 
-LML = -0.5*log(det(V)) - 0.5*Y'*V_inv*Y - 0.5*nnum*log(2*pi)
+%LML = -0.5*log(det(V)) - 0.5*Y'*V_inv*Y - 0.5*nnum*log(2*pi)
+LML = -0.5*log(det(V)) - 0.5*Y'*CholeskySolve(L,Y) - 0.5*nnum*log(2*pi);
+disp(LML)
 
 LML_array=[];
 
