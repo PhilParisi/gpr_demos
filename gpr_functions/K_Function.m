0a083d6756640f76fe42b70ed83856b625fd1de5
Phil Parisi <philsbeginnercode@gmail.com>
2022-07-07 10:40:35 -0400

    Updated the K_Function for clarity and switching based on 2D or 3D inputs. Added calcLML function for all functions to use

--- gpr_functions/K_Function.m
+++ gpr_functions/K_Function.m
@@ -1,6 +1,8 @@
 function [K] = K_Function(A, B, hp)
 % Run input vectors through the Kernel!
 % Inputs: A, B, and hp (vectors must be vertical, e.g. 400x2)
+% 2D --> Inputs are A (mx1) and B (nx1). Output is mxn.
+% 3D --> Inputs are A (mx2) and B (nx2). Output is mxn.
 
 
 %%%%% Error Message if matrix is not in right orientation (should be vertical)
@@ -13,27 +15,22 @@ end
 K = zeros(size(A,1),size(B,1)); %predefine K for code efficiency
     % size(X,1) gives rows, size(X,2) gives cols
 
-% 2D
-    % Inputs A is mx1 and B is nx1 (vectors)
-    %   Output will be mxn
-
-% 3D
-    % Inputs A is mx2 and B is nx2 (vectors)
-    %   Output will be mxn
-
+% Loop thru all indices and calculate kernel values
 for i = 1:length(A)             %Start with rows of A, i = 1, 2, ...
     for j = 1:length(B)         %Go thru cols of B, j = 1, 2, ...
         
         % Choose Kernel specified in argument
         if hp.kerneltype == "exact"
             K(i,j) = SqExpKernel(A(i,:), B(j,:), hp);
+
         elseif hp.kerneltype == "sparse"
             K(i,j) = SqExpKernelSparse(A(i,:), B(j,:), hp);
-        else %exact
+
+        else % other
             disp("Incorrect Kernel Type (sparse or exact) specified")
             return
-        end
 
+        end
     end
 end
 
